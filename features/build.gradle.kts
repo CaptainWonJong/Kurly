35f6da47d1f0241d92d71e9dbd4a31b2c655f5f7
CaptainWonJong <CaptainWonJong@gmail.com>
2023-05-02 17:54:20 +0900

    features 모듈 추가

---
+++ features/build.gradle.kts
@@ -0,0 +1,23 @@
+import extensions.*
+import org.gradle.kotlin.dsl.implementation
+import plugins.applyComposeConfig
+import plugins.applyHiltPluginConfig
+
+plugins {
+    id("kotlin-parcelize")
+}
+
+applyHiltPluginConfig()
+applyComposeConfig()
+
+dependencies {
+    DomainModule
+
+    addHiltDependencies()
+    addComposeDependencies()
+    implementation(libs.io.coil.kt.compose)
+}
+
+android {
+    namespace = "com.kurly.features"
+}
